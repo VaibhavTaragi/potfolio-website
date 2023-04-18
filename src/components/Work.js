4bdbecb8c6fccdc4948c949247201ed1623b181c
VaibhavTaragi <taragivaibhav@gmail.com>
2023-04-18 18:32:22 +0530

    Projects added

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,6 +1,8 @@
 import React from "react";
-import YT from '../assets/YT.PNG';
-import Movies from '../assets/Movies.PNG';
+import YT from "../assets/YT.PNG";
+import Movies from "../assets/Movies.PNG";
+import Dashboard from "../assets/dashboard.PNG";
+import FireAuth from '../assets/firebase-auth.PNG';
 
 const Work = () => {
   return (
@@ -15,36 +17,131 @@ const Work = () => {
 
         {/* Container */}
         <div className="grid sm:grid-cols-2 md:grid-cols-2 gap-4">
-
-            {/* Grid Item */}
-            <div style={{background: `url(${YT})` }} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-                {/* Hover effect */}
-                <div className="opacity-0 group-hover:opacity-100">
-                    <span className="text-2xl font-bold text-white text-center">Video Streaming Platform</span>
-                    <div className="pt-8 text-center">
-                        <a href='https://github.com/VaibhavTaragi/NamasteDEV-Youtube' target="_blank" rel="noreferrer">
-                            <button className="text-center rounded-lg px-4 p-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
-                        </a>
-                        <a href='https://namastedev-youtube.netlify.app/' target="_blank" rel="noreferrer">
-                            <button className="text-center rounded-lg px-4 p-3 m-2 bg-white text-gray-700 font-bold text-lg">Live</button>
-                        </a>                        
-                    </div>
-                </div>
+          {/* Grid Item */}
+          <div
+            style={{ background: `url(${YT})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover effect */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white text-center">
+                Video Streaming Platform
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://github.com/VaibhavTaragi/NamasteDEV-Youtube"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 p-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+                <a
+                  href="https://namastedev-youtube.netlify.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 p-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Live
+                  </button>
+                </a>
+              </div>
             </div>
-            <div style={{background:`url(${Movies})` }} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-                {/* Hover effect */}
-                <div className="opacity-0 group-hover:opacity-100">
-                    <span className="text-2xl font-bold text-white tracking-wider">Movies Info App</span>
-                    <div className="pt-8 text-center">
-                        <a href='https://github.com/VaibhavTaragi/movie-app' target="_blank" rel="noreferrer">
-                            <button className="text-center rounded-lg px-4 p-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
-                        </a>
-                        <a href='https://movie-info-react.netlify.app/' target="_blank" rel="noreferrer">
-                            <button className="text-center rounded-lg px-4 p-3 m-2 bg-white text-gray-700 font-bold text-lg">Live</button>
-                        </a>                        
-                    </div>
-                </div>
+          </div>
+          <div
+            style={{ background: `url(${Dashboard})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover effect */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white text-center">
+                User Dashboard
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://github.com/VaibhavTaragi/user-dashboard"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 p-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+                <a
+                  href="https://user-dashboard-nextjs.netlify.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 p-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Live
+                  </button>
+                </a>
+              </div>
             </div>
+          </div>
+          <div
+            style={{ background: `url(${FireAuth})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover effect */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white text-center">
+                User Authentication using Firebase
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://github.com/VaibhavTaragi/React-FirebaseAuth"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 p-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+                <a
+                  href="https://react-firebase-auth-breeze-ai.netlify.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 p-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Live
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ background: `url(${Movies})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover effect */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Movies Info App
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://github.com/VaibhavTaragi/movie-app"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 p-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+                <a
+                  href="https://movie-info-react.netlify.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 p-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Live
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
